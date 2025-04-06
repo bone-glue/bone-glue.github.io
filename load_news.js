ac3f55c5b9c4e5071ce568528835a1b52ab43c33
bone-glue <ichtchenkomaxim@gmail.com>
2025-04-05 21:19:47 -0400

    oops forgot to save

--- load_news.js
+++ load_news.js
@@ -4,7 +4,8 @@ const newsBlob = document.createElement("img");
 newsBlob.classList.Add("news-blob");
 newsBlob.src = "news/news_blob.png";
 const moodTextDiv = document.createElement("div");
-
+moodTextDiv.classList.Add("mood-text-div");
+const moodTextIndiv = document.createElement
 for (const curobj of newsList) {
     
 }
