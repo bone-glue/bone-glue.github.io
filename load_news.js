93fa5a9803d1880d55d6938c27bca1431bf5f32c
bone-glue <ichtchenkomaxim@gmail.com>
2025-04-05 21:17:59 -0400

    Started coding news generator

--- load_news.js
+++ load_news.js
@@ -1,3 +1,10 @@
+const newsItem = document.createElement("div");
+newsItem.classList.Add("news-item");
+const newsBlob = document.createElement("img");
+newsBlob.classList.Add("news-blob");
+newsBlob.src = "news/news_blob.png";
+const moodTextDiv = document.createElement("div");
+
 for (const curobj of newsList) {
     
 }
