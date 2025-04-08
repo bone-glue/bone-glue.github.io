012b29de317c63e6d36b8b62cb574d2cbf14ba39
bone-glue <ichtchenkomaxim@gmail.com>
2025-04-08 19:24:28 -0400

    Finished News_load Script

--- load_news.js
+++ load_news.js
@@ -1,11 +1,3 @@
-const newsItem = document.createElement("div");
-newsItem.classList.Add("news-item");
-const newsBlob = document.createElement("img");
-newsBlob.classList.Add("news-blob");
-newsBlob.src = "news/news_blob.png";
-const moodTextDiv = document.createElement("div");
-moodTextDiv.classList.Add("mood-text-div");
-const moodTextIndiv = document.createElement
 for (const curobj of newsList) {
-    
+    createNewsItem(curobj.title,curobj.content,curobj.moodimg,curobj.mood,curobj.date);
 }
--- news_info.js
+++ news_info.js
@@ -1,13 +1,42 @@
 const news1 = {
     title: "Test Title",
     content: "<p>Test Content Test Content Test Content Test Content</p>",
-    moodimg: "happy.gif",
+    moodimg: "news_happy.gif",
     mood: "Inquizitive",
+    date: "4/4/25",
 };
 const news2 = {
     title: "Puppies 2",
     content: "<p>Large Puppies Are Sweet And Big and Sometimes Loud</p>",
-    moodimg: "shy.gif",
+    moodimg: "news_shy.gif",
     mood: "Doggy",
+    date: "4/8/25",
 };
 const newsList = [ news1,news2 ];
+function createNewsItem(title,content,moodimg,mood,date) {
+    let newsItem = document.getElementById("news").appendChild(document.createElement("div"));
+    newsItem.classList.add("news-item");
+    let newsBlob = newsItem.appendChild(document.createElement("img"));
+    let newsFace = newsItem.appendChild(document.createElement("img"));
+    newsFace.classList.add("news-face");
+    newsFace.src = "news/" + moodimg;
+    newsBlob.classList.add("news-blob");
+    newsBlob.src = "news/news_blob.png";
+    let moodTextDiv = newsItem.appendChild(document.createElement("div"));
+    moodTextDiv.classList.add("mood-text-div");
+    let moodText = moodTextDiv
+        .appendChild(document.createElement("div"))
+        .appendChild(document.createElement("p"));
+    let newsRec = newsItem.appendChild(document.createElement("div"));
+    newsRec.classList.add("news-rec");
+    let newsTitle = document.createElement("h2");
+    let newsCont = document.createElement("div");
+    newsRec.appendChild(newsCont);
+    newsRec.insertBefore(newsTitle,newsCont);
+    let newsDate = newsItem.appendChild(document.createElement("p"));
+    newsDate.classList.add("date");
+    newsCont.innerHTML = content;
+    newsTitle.textContent = title;
+    moodText.innerHTML = "<u>Feelin':<u><br>" + mood;
+    newsDate.textContent = date;
+}
