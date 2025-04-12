eecec2729c7b4d61837f30b20dc93ab996384397
bone-glue <ichtchenkomaxim@gmail.com>
2025-04-11 21:36:26 -0400

    added music button functionality

--- base_script.js
+++ base_script.js
@@ -3,8 +3,12 @@ function toggleMusic() {
     music = !music;
     if (music) {
         document.getElementById("music-guy").src = "music_on.svg";
+        document.getElementById("music-status").innerHTML = "PLAYING";
+        document.getElementById("music").play();
     } 
     else {
         document.getElementById("music-guy").src = "music_off.svg";
+        document.getElementById("music-status").innerHTML = "PAUSED";
+        document.getElementById("music").pause();
     }
 }
