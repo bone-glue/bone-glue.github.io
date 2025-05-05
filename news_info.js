05d1fce6d46fe7f1a6b6e741ce2c223e45a8f633
bone-glue <ichtchenkomaxim@gmail.com>
2025-05-05 15:27:51 -0400

    Finished Website
    Updated Bug Squash
    Added direct contact to email
    added contact me section to all pages but game
    
    This is Timp

--- news_info.js
+++ news_info.js
@@ -1,46 +1,12 @@
 const newsArray = [
     {
-        title: "Website Making Progress!",
-        content: "<p>Thats right folks! There has been a lot of progress on this website so far! Just a couple more thing left to make :-).</p>",
+        title: "Welcome to my New Website!",
+        content: "<p>Heya! If you're reading this post, that means youv not only found yourself on my humble, hand-crafted website, but you also have begun to explore it. Thank you!</p> <hr> <p>This website will serve as the cornerstone of all my creative projects, allowing you to follow updates, view my ever updating gallary of art pieces, and maybe more...? You can also contact me, whether you are a potential client, or you just wanted to reach out and say anything :v), my inbox is open. On top of that, there seems to be something odd in the 'GAME' page, maybe you can scout it out? (I'm too scared to)</p> <hr> <p>Happy browsing!</p>",
         moodimg: "news_happy.png",
         mood: "Excited!",
-        date: "4/4/25",
+        date: "5/5/25",
     },
-    {
-        title: "Puppies 2",
-        content: "<p>Large Puppies Are Sweet And Big and Sometimes Loud</p>",
-        moodimg: "news_love.png",
-        mood: "Puppy Love",
-        date: "4/8/25",
-    },
-    {
-        title: "Explode this Person",
-        content: "<p>So I saw someone who shoved a large dog into the infinte suffer pit and liiiiiiiiiike... wierd right?</p> <hr> <p>So I'm making an executive order to have the man exploded exploded exploded, head to knee exploded. Whatever.</p>",
-        moodimg: "news_roll.png",
-        mood: "Fed. Up.",
-        date: "4/24/25",
-    },
-    {
-        title: "Used the Toilet",
-        content: "<p>Had a tinkle winkle! Just a binkle dinkle :-)</p>",
-        moodimg: "news_happy.png",
-        mood: "Tee Hee",
-        date: "The first thing ever.",
-    },
-    {
-        title: "My Peons Continue to Defy Me",
-        content: "<p>I'm not getting enough LIKES on my ART. I just want MONEY and to be FAMOUS. That's all art means to me... heh heh heh...</p><p>*peels grapes* touch this.</p>",
-        moodimg: "news_mad.png",
-        mood: "Betrayed",
-        date: "99/99/9999",
-    },
-    {
-        title: "im not mad",
-        content: "<p>I am not mad, stop posting that I'm mad Im actulaly so chill</p><br> <p>stop</p>",
-        moodimg: "news_cool.png",
-        mood: "im so good",
-        date: "1",
-    },
+    
 ];
 function createNewsItem(title,content,moodimg,mood,date) {
     let newsItem = document.getElementById("news-reel").appendChild(document.createElement("div"));
